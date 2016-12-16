5f8204d2f2e7163941a2104915c975585750c936
akholod <andrey.kokhanevich@gmail.com>
2016-12-16 10:29:08 +0200

    add book searching with animation

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,39 @@
 'use strict';
 
-const app = angular.module('myApp', []);
+const app = angular.module('myApp', ['ngAnimate']);
 
-app;
+app.service('BooksCatalog', ['$http', function($http){
+    this.getBooks = function() {
+        return $http({
+            method: 'GET',
+            url: '/api/books/'
+        })
+        .then((response) => {
+            return response;
+        }, (dataError) => {
+            new Error((dataError));
+        })
+    }
+}]);
+
+app.controller('BooksCtrl',['BooksCatalog', function(BooksCatalog) {
+
+    BooksCatalog.getBooks().then((response) => {
+        this.books = response.data;
+        console.log(this.books);
+    });
+
+
+}]);
+
+app.directive('mainBooksCatalog', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'templates/main-books-catalog.html',
+        controller: 'BooksCtrl',
+        controllerAs: 'booksCtrl',
+    };
+});
+
+
