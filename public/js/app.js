b119d994b43e66f7b0a259ab6844d94497f9776c
akholod <andrey.kokhanevich@gmail.com>
2016-12-24 23:50:28 +0200

    add templates with login, signup and search fields

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,20 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider, Resta
         .state('book', {
             url: '/books/:bookId',
             template: '<book-card></book-card>'
+        })
+        .state('login', {
+            url: '/login',
+            template: '<login-form></login-form>'
+        })
+        .state('signup', {
+            url: '/signup',
+            template: '<signup-form></signup-form>'
+        })
+        .state('searchbook', {
+            url: '/searchbook',
+            template: '<add-books-form></add-books-form>'
         });
+
     $urlRouterProvider.otherwise('/books');
 
     RestangularProvider.setBaseUrl("http://localhost:3000/api");
@@ -49,13 +62,35 @@ app.service('BooksActions', ['Restangular', function(Restangular) {
     };
 }]);
 
+app.service('BookSearch', ['$http', function($http) {
+    this.findBooks = function (searchBookTitle, searchBookLang = 'en') {
+        return $http.post('api/books/search', {
+            "bookTitle" : searchBookTitle,
+            "bookLang" : searchBookLang,
+        }).then((response) => {
+            console.log(response)
+            return response;
+        }, (dataError) => {
+            new Error((dataError));
+        });
+    }
+}]);
+
+app.service('addBookToUserBooks', ['Restangular', function(Restangular) {
+    this.addBookToUserBooks = function(bookData) {
+        return Restangular.one('books', bookId).put()
+            .then((response) => {
+                return response;
+            }, (dataError) => {
+                new Error((dataError));
+            });
+    };
+}]);
+
 app.controller('BooksCtrl', ['BooksCatalog', 'BooksActions', function(BooksCatalog, BooksActions) {
     BooksCatalog.getBooks().then((response) => {
         this.books = response;
     });
-
-
-
 }]);
 
 app.controller('BookDescriptionCtrl', ['BooksCatalog', '$stateParams', function(BooksCatalog, $stateParams) {
@@ -63,6 +98,16 @@ app.controller('BookDescriptionCtrl', ['BooksCatalog', '$stateParams', function(
         this.book = response;
     });
 }]);
+
+app.controller('AddBooksCtrl', ['BookSearch', function(BookSearch) {
+    this.searchBook = function () {
+        BookSearch.findBooks(this.bookSearchField, this.bookSearchLang).then((response) => {
+
+        });
+    };
+}]);
+
+
 
 app.directive('mainBooksCatalog', function() {
     return {
@@ -74,7 +119,6 @@ app.directive('mainBooksCatalog', function() {
     };
 });
 
-
 app.directive('bookCard', function() {
     return {
         restrict: 'E',
@@ -82,5 +126,34 @@ app.directive('bookCard', function() {
         templateUrl: 'templates/book-card.html',
         controller: 'BookDescriptionCtrl',
         controllerAs: 'bookDescriptionCtrl',
+    };
+});
+
+app.directive('addBooksForm', function () {
+   return {
+       restrict: 'E',
+       scope: {},
+       templateUrl: 'templates/add-books-form.html',
+       controller: 'AddBooksCtrl',
+       controllerAs: 'addBooksCtrl',
+   }
+});
+
+app.directive('signupForm', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'templates/signup-form.html',
+        controller: 'SignupCtrl',
+        controllerAs: 'signupCtrl',
+    };
+});
+app.directive('loginForm', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'templates/login-form.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'loginCtrl',
     };
 });
