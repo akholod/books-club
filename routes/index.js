bec9053ca4af36768675b30a68d5931b90999b8b
akholod <andrey.kokhanevich@gmail.com>
2017-01-10 17:43:20 +0200

    rename client part and fix client dependency

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,4 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-/*
-//home page
-router.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname, 'public', 'index'));
-});
-*/
-
 module.exports = router;
