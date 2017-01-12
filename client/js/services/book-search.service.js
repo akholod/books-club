63d932eb053ca8e8e7511fdeef348b86f8d8ae6d
akholod <andrey.kokhanevich@gmail.com>
2017-01-12 17:42:12 +0200

    production build

--- client/js/services/book-search.service.js
+++ client/js/services/book-search.service.js
@@ -6,6 +6,14 @@ module.exports = function($http) {
             "bookTitle" : searchBookTitle,
             "bookLang" : searchBookLang,
         }).then((response) => {
+            let arr = [];
+            response.data.forEach((item) => {
+                item.thumbnail = item.thumbnail.replace("http:","https:");
+                arr.push(item)
+            });
+            response.data = arr;
+            console.log(arr);
+            console.log(response);
             return response;
         }, (dataError) => {
             new Error((dataError));
